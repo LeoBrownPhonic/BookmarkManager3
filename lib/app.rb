5830898a555630bddfd688958197824d7da66c95
pinar <pnr_ozbakir@msn.com>
2021-10-11 17:17:48 +0100

    Add Bookmark class and refactor code

--- lib/app.rb
+++ lib/app.rb
@@ -1,13 +1,19 @@
 require "sinatra/base"
 require "sinatra/reloader"
+require_relative './bookmark'
 
-class BookMark < Sinatra::Base
+class BookmarkManager < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
 
   get "/" do
     "Welcome to Bookmark Manager"
+  end
+
+  get "/bookmarks" do
+    @bookmarks = Bookmark.all
+    erb :bookmarks
   end
 
   # start the server if ruby file executed directly
