6b8646c2648be9f3b8848bd84473413ef531b441
Leo Brown <leo_brown_1@hotmail.com>
2021-10-13 18:10:36 +0100

    Step 11 complete

--- spec/features/creating_bookmarks_spec.rb
+++ spec/features/creating_bookmarks_spec.rb
@@ -2,8 +2,9 @@ feature "Adding a new bookmark" do
   scenario "A user can add a bookmark to bookmark manager" do
     visit("/bookmarks/new")
     fill_in("url", with: "http://example.org")
+    fill_in("title", with: "example")
     click_button("Submit")
 
-    expect(page).to have_content "http://example.org"
+    expect(page).to have_content "example"
   end
 end
